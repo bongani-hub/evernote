f65d6d74b667d118ba49ef99c60ed9116606f820
bongani-hub <94044452+bongani-hub@users.noreply.github.com>
2022-05-19 11:29:43 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB5NfoyH9ofXvbqEgNrSlfE_hN2Efw7rK8",
+  authDomain: "evernote-clone-a32eb.firebaseapp.com",
+  projectId: "evernote-clone-a32eb",
+  storageBucket: "evernote-clone-a32eb.appspot.com",
+  messagingSenderId: "153281620509",
+  appId: "1:153281620509:web:40d50310cc5c296dc9f4f4",
+  measurementId: "G-5F7N8H4LMW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
